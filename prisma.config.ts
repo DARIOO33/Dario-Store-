41f5d4432b85eee057786e9dd6a67ab7017c888f
DARIOO33 <darioanwar3@gmail.com>
2026-09-25 16:28:09 +0100

    prisma.config.ts updates

--- prisma.config.ts
+++ prisma.config.ts
@@ -8,7 +8,7 @@ export default definePrismaConfig({
   orm: ormConfig({
     contract: "./src/prisma/contract.prisma",
     db: {
-      connection: "postgresql://postgres:postgres123@localhost:5432/membership",
+      connection: process.env.DATABASE_URL ?? "postgresql://postgres:postgres123@localhost:5432/membership",
     },
   }),
   composer: {
